49ca7330b0b8ab7fc699a0118ed02d370960bed4
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-06-22 23:53:21 +0530

    Create Weird_Algorithm.cpp

---
+++ Weird Algorithm/Weird_Algorithm.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define int ll
+ 
+signed main()
+{
+    int n, i, k;
+    cin >> n;
+    k = n;
+    for(;;)
+    {
+        if(k == 1)
+        {
+            cout << k;
+            break;
+        }
+        cout << k << " ";
+        if(k % 2)
+        {
+            k = 3 * k + 1;
+        }
+        else
+        {
+            k = k / 2;
+        }
+    }
+}
