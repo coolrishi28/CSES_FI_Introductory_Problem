0e21f640656c359e26133297869ef8399e4d3931
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-06-28 23:30:07 +0530

    Create Creating_Strings.cpp

---
+++ Creating Strings/Creating_Strings.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define int ll
+const int mod = 1000000007;
+ 
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string str;
+    cin >> str;
+    vector<string> ans;
+    int count = 0, i, len = str.length();
+    sort(str.begin(), str.end());
+    
+    do{
+        count++;
+        ans.push_back(str);
+    }while(next_permutation(str.begin(), str.end()));
+    
+    cout << count << endl;
+    
+    for(i = 0; i < count; i++)
+    {
+        cout << ans[i] << endl;
+    }
+    
